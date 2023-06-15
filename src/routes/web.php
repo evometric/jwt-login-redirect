10a945d9dc077421fd1bfdcd13a69fec438845bd
Rob Shepherd <rs@datacymru.net>
2023-06-15 13:27:40 +0100

    Fix session issue by using web middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,7 @@
 use Evometric\JwtLoginRedirect\Controllers\JwtLoginRedirectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('jwt-login-redirect/{to}', [JwtLoginRedirectController::class, 'get'])->where('to', '.*'); 
-Route::get('jwt-login-redirect-test/{to}', [JwtLoginRedirectController::class, 'get_test'])->where('to', '.*'); 
+Route::middleware(['web'])->group(function () {
+    Route::get('jwt-login-redirect/{to}', [JwtLoginRedirectController::class, 'get'])->where('to', '.*'); 
+    Route::get('jwt-login-redirect-test/{to}', [JwtLoginRedirectController::class, 'get_test'])->where('to', '.*'); 
+});
